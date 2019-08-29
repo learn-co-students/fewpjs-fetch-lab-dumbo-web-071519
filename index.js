4fccb6e7bd751c1d668c898f83d4d18bda70c189
Ross Perkel <rossperkel@gmail.com>
2019-08-28 20:48:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json) )
+  
+  }
 
-}
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
