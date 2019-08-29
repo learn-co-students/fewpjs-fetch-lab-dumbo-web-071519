f77029110a28e16ac3c2c3024a5d9b0c337184c2
github username <github email address>
2019-08-29 12:46:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 function fetchBooks() {
+    return fetch("https://anapioficeandfire.com/api/books")
+        .then(response => response.json())
+        .then(json => renderBooks(json))
+}
 
+function getPosts(anything) {
+    renderBooks(anything);
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const main = document.querySelector('main');
+    json.forEach(bookele => {
+        const header = document.createElement('h2');
+        header.innerText = bookele.name;
+        main.appendChild(header);
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+    fetchBooks()
 })
